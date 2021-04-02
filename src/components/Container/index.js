39907877d15e9fa31cdae9ef6c43c064db9dd5c9
magdazelena <magda.zelezik@gmail.com>
2021-04-02 14:09:23 +0200

    Checking previous state

--- src/components/Container/index.js
+++ src/components/Container/index.js
@@ -1,7 +1,12 @@
 import React from 'react'
 
 export default function Container({ children }) {
+  const onVisibilityChange = (visible, id) => {
+    console.log(visible, id)
+  }
   return <div className="container">
-    {children}
+    {children.map((child, index) => {
+      return React.cloneElement(child, { onVisibilityChange })
+    })}
   </div>
 }
--- src/components/Section/index.jsx
+++ src/components/Section/index.jsx
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useInView } from 'react-intersection-observer'
 import { useStyle as StyledSection } from './useStyle'
 
 export default function Section(props) {
-  const { children, id } = props
+  const { children, id, onVisibilityChange } = props
+  const [visible, setVisible] = useState(false) //previous state
   const [ref, inView] = useInView({
-    threshold: 0.05
+    threshold: [0.05, 0.9]
   })
-  if (inView) console.log(inView, id)
+  useEffect(() => {
+    if (inView !== visible) {
+      setVisible(inView)
+      onVisibilityChange(inView, id)
+    }
+  }, [inView])
+
   return (
     <StyledSection ref={ref}>
       {children}
