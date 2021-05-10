7a71b304777081ddf7b8ba9aa2a4a56331b54feb
magdazelena <magda.zelezik@gmail.com>
2021-05-10 19:56:24 +0200

    Added snack messages

--- src/App.js
+++ src/App.js
@@ -1,33 +1,35 @@
 import React from 'react'
+import SnackbarProvider from 'react-simple-snackbar'
 import Container from './components/Container';
 import Section from './components/Section';
 
 function App() {
   return (
-    <Container>
-      <Section id="1">
-        <p>1</p>
-      </Section>
-      <Section id="2">
-        <p>2</p>
-      </Section>
-      <Section id="3" manual>
-        <Button />
-        <p>3</p>
-      </Section>
-      <Section id="4">
-        <p>4</p>
-      </Section>
-      <Section id="5">
-        <p>5</p>
-      </Section>
-      <Section id="6">
-        <p>6</p>
-      </Section>
-    </Container>
+    <SnackbarProvider>
+      <Container>
+        <Section id="1">
+          <p>1</p>
+        </Section>
+        <Section id="2">
+          <p>2</p>
+        </Section>
+        <Section id="3" manual>
+          <Button />
+        </Section>
+        <Section id="4">
+          <p>4</p>
+        </Section>
+        <Section id="5">
+          <p>5</p>
+        </Section>
+        <Section id="6">
+          <p>6</p>
+        </Section>
+      </Container>
+    </SnackbarProvider>
   );
 }
 
 export default App
 
-const Button = (props) => (<button onClick={props.resumeScroll}>Trigger</button>)
+const Button = (props) => (<button onClick={props.resumeScroll}>Click to resume scrolling</button>)
--- src/components/Container/index.js
+++ src/components/Container/index.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import { useSnackbar } from 'react-simple-snackbar'
 
 export default function Container({ children }) {
+  const [openTopbar] = useSnackbar({ position: 'top-right' })
+  const [openBottombar] = useSnackbar({ position: 'bottom-right' })
+
   const onVisibilityChange = (visible, id) => {
-    console.log(visible, id)
+    if (visible) openTopbar(`Section ${id} is visible`)
+    if (!visible) openBottombar(`Section ${id} is not visible`)
   }
   return <div className="container">
     {children.map((child, key) => {
